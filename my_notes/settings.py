6ff7beace638defbe349972217738a0b116906c7
yuanyehome <2252706531@qq.com>
2020-01-28 18:40:53 +0800

    use mysql

--- my_notes/settings.py
+++ my_notes/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'my_notes.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_exercise',
+        'USER': 'root',
+        'PASSWORD': 'yuanye985962',
+        'HOST': 'cdb-ptgvrpkc.bj.tencentcdb.com',
+        'PORT': '10200'
     }
 }
 
